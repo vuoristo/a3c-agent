d679fa8608636c97a158a886a217691ea6d45a07
Risto Vuorio <vuoristo@gmail.com>
2016-12-06 19:54:19 +0200

    Changes to ThreadModel to enable more advanced models
    ThreadModel class modified to support storing global model and gradient
    moving averages.
    
    Instead of manually collecting trainable variables in lists,
    variable_scopes are used.
    
    These two changes make the model cleaner, make it easier to change the
    models and even more importantly allow the use of LSTM or other RNN
    cells, that don't have explicit interfaces for collecting trainable
    variables manually.

--- lol-agent.py
+++ lol-agent.py
@@ -13,64 +13,71 @@ def categorical_sample(prob_n):
   csprob_n = np.cumsum(prob_n)
   return (csprob_n > np.random.rand()).argmax()
 
-def build_network(input_size, hidden_1, hidden_2, output_size):
-  init = tf.uniform_unit_scaling_initializer()
-  W0 = tf.get_variable('W0', shape=(input_size, hidden_1), initializer=init)
-  b0 = tf.get_variable('b0', initializer=tf.constant(0., shape=(hidden_1,)))
-
-  W_softmax = tf.get_variable('W_softmax', shape=(hidden_1, output_size),
-    initializer=init)
-  b_softmax = tf.get_variable('b_softmax', initializer=tf.constant(0.,
-    shape=(output_size,)))
-
-  W_linear = tf.get_variable('W_linear', shape=(hidden_1, 1), initializer=init)
-  b_linear = tf.get_variable('b_linear', initializer=tf.constant(0.,
-    shape=(1,)))
-
-  pol_vars = [W0, b0, W_softmax, b_softmax]
-  val_vars = [W_linear, b_linear, W0, b0]
-
-  return pol_vars, val_vars
-
 class ThreadModel(object):
-  def __init__(self, nO, nA, global_policy, global_value, pol_grad_msq,
-               val_grad_msq, config):
-    self.ob = tf.placeholder(tf.float32, (None, nO), name='ob')
+  def __init__(self, input_size, output_size, global_network, is_global, config):
+    self.ob = tf.placeholder(tf.float32, (None, input_size), name='ob')
     self.ac = tf.placeholder(tf.int32, (None, 1), name='ac')
     self.rew = tf.placeholder(tf.float32, (None, 1), name='rew')
     self.lr = tf.placeholder(tf.float32, name='lr')
 
-    pol_vars, val_vars = build_network(nO, config['hidden_1'],
-      config['hidden_2'], nA)
+    W0_size = config['hidden_1']
 
-    h_1 = tf.tanh(tf.nn.bias_add(tf.matmul(
-      self.ob, pol_vars[0]), pol_vars[1]))
+    with tf.variable_scope('shared_variables') as sv_scope:
+      init = tf.uniform_unit_scaling_initializer()
+      W0 = tf.get_variable('W0', shape=(input_size, W0_size), initializer=init)
+      b0 = tf.get_variable('b0', initializer=tf.constant(0., shape=(W0_size,)))
 
-    self.pol_prob = tf.nn.softmax(tf.nn.bias_add(tf.matmul(h_1, pol_vars[2]),
-      pol_vars[3]))
-    self.val = tf.nn.bias_add(tf.matmul(h_1, val_vars[0]), val_vars[1])
+    with tf.variable_scope('policy') as pol_scope:
+      W_softmax = tf.get_variable('W_softmax', shape=(W0_size, output_size),
+        initializer=init)
+      b_softmax = tf.get_variable('b_softmax', initializer=tf.constant(0.,
+        shape=(output_size,)))
 
-    ac_oh = tf.reshape(tf.one_hot(self.ac, nA), (-1, nA))
-    masked_prob_na = tf.reduce_sum(ac_oh * self.pol_prob, reduction_indices=1)
-    score = tf.mul(tf.log(tf.clip_by_value(
-      masked_prob_na, 1.e-10, 1.0)), self.rew - self.val)
-    value_loss = tf.nn.l2_loss(self.rew - self.val)
+    with tf.variable_scope('value') as val_scope:
+      W_linear = tf.get_variable('W_linear', shape=(W0_size, 1),
+        initializer=init)
+      b_linear = tf.get_variable('b_linear', initializer=tf.constant(0.,
+        shape=(1,)))
 
-    self.pol_grads = tf.gradients(score, pol_vars)
-    self.val_grads = tf.gradients(value_loss, val_vars)
+    shared_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+      scope=sv_scope.name)
+    self.pol_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+      scope=pol_scope.name) + shared_vars
+    self.val_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+      scope=val_scope.name) + shared_vars
 
-    self.pol_updates = self.get_updates(global_policy, pol_vars,
-        self.pol_grads, pol_grad_msq)
-    self.val_updates = self.get_updates(global_value, val_vars,
-        self.val_grads, val_grad_msq)
+    if is_global:
+      self.pol_grad_msq = [tf.Variable(np.zeros(var.get_shape(),
+        dtype=np.float32)) for var in self.pol_vars]
+      self.val_grad_msq = [tf.Variable(np.zeros(var.get_shape(),
+        dtype=np.float32)) for var in self.val_vars]
 
-    self.dbg_var = self.pol_updates[0]
+    if not is_global:
+      h_1 = tf.tanh(tf.nn.bias_add(tf.matmul(self.ob, W0), b0))
 
-  def get_updates(self, global_vars, local_vars, grads, grad_msq, momentum=0.9,
-                  epsilon=1e-9):
+      self.pol_prob = tf.nn.softmax(tf.nn.bias_add(tf.matmul(h_1, W_softmax),
+        b_softmax))
+      self.val = tf.nn.bias_add(tf.matmul(h_1, W_linear), b_linear)
+
+      ac_oh = tf.reshape(tf.one_hot(self.ac, output_size), (-1, output_size))
+      masked_prob_na = tf.reduce_sum(ac_oh * self.pol_prob, reduction_indices=1)
+      score = tf.mul(tf.log(tf.clip_by_value(
+        masked_prob_na, 1.e-10, 1.0)), self.rew - self.val)
+      value_loss = tf.nn.l2_loss(self.rew - self.val)
+
+      self.pol_grads = tf.gradients(score, self.pol_vars)
+      self.val_grads = tf.gradients(value_loss, self.val_vars)
+
+      self.pol_updates = self.get_updates(global_network.pol_vars,
+        self.pol_vars, self.pol_grads, global_network.pol_grad_msq)
+      self.val_updates = self.get_updates(global_network.val_vars,
+        self.val_vars, self.val_grads, global_network.val_grad_msq)
+
+  def get_updates(self, global_vars, local_vars, grads, grad_msq,
+                  momentum=0.9, epsilon=1e-9):
     updates = []
     for Wg, Wl, grad, msq in zip(global_vars, local_vars, grads, grad_msq):
-      grad = tf.clip_by_norm(grad, 10.)
+      grad = tf.clip_by_norm(grad, 1.)
 
       # compute rmsprop update per variable
       ms_update = momentum * msq + (1. - momentum) * tf.pow(grad, 2)
@@ -97,26 +104,20 @@ class LOLAgent(object):
         min_lr = 0.002,
         lr_decay_steps = 10000,
         hidden_1 = 20,
-        hidden_2 = 5,
         episode_max_length = 100,
         num_threads = 1,
       )
 
     self.config.update(usercfg)
 
-    with tf.variable_scope('global_network') as global_scope:
-      pol_vars, val_vars = build_network(self.nO, self.config['hidden_1'],
-        self.config['hidden_2'], self.nA)
-      pol_grad_msq = [tf.Variable(np.zeros(var.get_shape(), dtype=np.float32))
-        for var in pol_vars]
-      val_grad_msq = [tf.Variable(np.zeros(var.get_shape(), dtype=np.float32))
-        for var in val_vars]
+    with tf.variable_scope('global'):
+      global_model = ThreadModel(self.nO, self.nA, None, True, self.config)
 
     self.thr_models = []
     for thr in range(self.config['num_threads']):
       with tf.variable_scope('thread_{}'.format(thr)):
-        thr_model = ThreadModel(self.nO, self.nA, pol_vars,
-          val_vars, pol_grad_msq, val_grad_msq, self.config)
+        thr_model = ThreadModel(self.nO, self.nA, global_model, False,
+            self.config)
         self.thr_models.append(thr_model)
 
     self.sess = tf.Session()
