Fix value_loss gradient
Gradient computed for policy_loss shouldn't go through val.

Python literal arithmetic is evaluated before tensorflow operations. X
** 2 / 2 == X ** 1 if X is a tensor.

acagent.py
@@ -113,11 +113,11 @@ class ThreadModel(object):
         masked_prob = tf.reduce_sum(actions_one_hot * self.pol_prob,
           reduction_indices=1, keep_dims=True)
         log_masked_prob = tf.log(tf.clip_by_value(masked_prob, 1.e-22, 1.0))
-        td_error = self.rew - self.val
+        td_error = tf.identity(self.rew - self.val)
         entropy = -tf.reduce_sum(masked_prob * log_masked_prob,
           reduction_indices=1, keep_dims=True) * entropy_beta
         policy_loss = -(log_masked_prob * td_error + entropy)
-        value_loss = td_error ** 2. / 2.
+        value_loss = 0.5 * (self.rew - self.val) ** 2.
         total_loss = policy_loss + 0.5 * value_loss
 
       with tf.name_scope('gradients'):